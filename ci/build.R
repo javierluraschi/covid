4449bffc95ac87eb98a5fbc93b1d8f0d48ccc328
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-28 15:37:21 -0800

    parse and clean report tables

--- ci/build.R
+++ ci/build.R
@@ -1,5 +1,10 @@
 source("R/index.R")
 index <- covid19_index()
 
+report <- NULL
+for (entry in index) {
+  report <- rbind(report, covid19_parse(entry))
+}
+
 pins::board_register_github(repo = "javierluraschi/covid19", branch = "datasets")
-pins::pin(index, "covid19", board = "github")
+pins::pin(report, "covid19", board = "github")
