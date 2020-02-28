b82a98fcaade50d01a8281df6866aaa138b5e3ca
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-28 11:40:52 -0800

    read index of covid19 reports

---
+++ R/index.R
@@ -0,0 +1,11 @@
+covid19_index <- function() {
+  base_path <- "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports"
+
+  index <- xml2::read_html(base_path)
+
+  pdfs <- rvest::html_nodes(index, 'body') %>% xml2::xml_find_all("//a[contains(@href,'.pdf')]")
+
+  links <- paste0("https://www.who.int", rvest::html_attr(pdfs, "href"))
+
+  data.frame(link = links, stringsAsFactors = FALSE)
+}
