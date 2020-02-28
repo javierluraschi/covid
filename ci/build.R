5881fb7caba3cbe8d03fa787dd74926804fdf5f9
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-28 12:21:32 -0800

    push dataset as pin

--- ci/build.R
+++ ci/build.R
@@ -1,2 +1,5 @@
-source("../R/index.R")
-covid19_index()
+source("R/index.R")
+index <- covid19_index()
+
+pins::board_register_github(repo = "javierluraschi/covid19", token = Sys.getenv("GITHUB_TOKEN"), branch = "datasets")
+pins::pin(index, "covid19", board = "github")
