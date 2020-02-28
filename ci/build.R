2a63790bb595b25a516b8ab8111a90da3eccec03
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-28 13:12:55 -0800

    use github pat to push dataset

--- ci/build.R
+++ ci/build.R
@@ -1,5 +1,5 @@
 source("R/index.R")
 index <- covid19_index()
 
-pins::board_register_github(repo = "javierluraschi/covid19", token = Sys.getenv("GITHUB_TOKEN"), branch = "datasets")
+pins::board_register_github(repo = "javierluraschi/covid19", branch = "datasets")
 pins::pin(index, "covid19", board = "github")
