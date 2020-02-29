287cf2d0d1afe217a70308485a08c9f4d3ab516f
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-28 18:50:14 -0800

    write raw tables

--- ci/build.R
+++ ci/build.R
@@ -1,11 +1,27 @@
 sapply(dir("R", full.names = T), source)
 
+pins::board_register_github(repo = "javierluraschi/covid", branch = "datasets")
+
+index_processed <- c()
 index <- covid19_index()
 
-report <- NULL
-for (entry in index) {
-  report <- rbind(report, covid19_parse(entry))
+if (nrow(pins::pin_find("covid-index", board = "github")) > 0) {
+  index_processed <- pins::pin_get("covid-index", board = "github")
 }
 
-pins::board_register_github(repo = "javierluraschi/covid19", branch = "datasets")
-pins::pin(report, "covid19", board = "github")
+index <- index[!index %in% index_processed]
+
+for (entry in index) {
+  report_date <- gsub(".*situation-reports/|\\-.*", "", entry)
+  tables <- covid19_parse(entry)
+
+  # remove lists from tables
+  tables <- Filter(function(e) !any(grepl("•", e[,1])), tables)
+
+  for (table_num in seq_along(tables)) {
+    name <- paste0("covid-", report_date, "-", table_num)
+    data <- tibble::as_tibble(as.data.frame(tables[[table_num]], stringsAsFactors = FALSE))
+
+    pins::pin(data, name = name, board = "github")
+  }
+}
